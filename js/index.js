9dad5ee59797be691452c2d77d286737e989d22d
mepox <21198248+mepox@users.noreply.github.com>
2022-09-01 08:04:17 +0100

    change layout

--- js/index.js
+++ js/index.js
@@ -3,46 +3,36 @@ function onStart() {
 	
 	addTitle("Projects");
 	
-	var projectGroupTitles = [ "Commercial Games", "Recent projects on GitHub" ];
+	var projectGroupTitles = [ "Recent projects on GitHub", "Commercial Projects" ];
 	generateProjectGroups(projectGroupTitles);
 	
-	/* Brick Maniac */
+		/* URL Shortener */
 	
-	var title = "Brick Maniac (2014)";
-	var shortdesc = "An Android game developed in a team of two.";
-	var longdesc =  "<p>The goal was to make a classic brick breaker game which is much more polished than the rest of the similar games in the Play Store. " + 
-					"We made a free version of the game which contained banner ads and limited levels. " + 
-					"Also we had a paid (pro) version which was completely ad free and all the levels were available. </p>" +
-					"<p>I was responsible to plan out the game's framework and to develop half of the game's code. " +
-					"Also, as I was in charge to manage our Play Store's Developer Account I had to build a separate package (free and paid) from the game and upload them.</p>" +
-					"<p>We have reached about 500 downloads.</p>" +
+	var title = "URL Shortener";
+	var shortdesc = "A simple URL Shortener made with Spring Boot.";
+	var longdesc = "<p></p>" +
 					"<p><label><b>Tech Summary:</b></label>" +
 					"<ul>" +
-					"<li>Language: Java (Android)</li>" +
-					"<li>Game engine: Written from scratch</li>" +
-					"<li>Version control: SVN</li></ul></p>";
-	var directory ="projects/brickmaniac";
-	var photosName = [ "photo_1.jpg", "photo_2.jpg", "photo_3.png"];
+					"<li>Language: Java</li>" +
+					"<li>Framework: Spring Boot</li>" +
+					"<li>Version control: Git</li></ul></p>";
+	var directory = "projects/urlshortener";
+	var photosName = [ "screenshot.jpg" ];
 	
 	addProject(0, title, shortdesc, longdesc, directory, photosName);
 	
-	/* Bouncy Ball */
+	/* Workout Tracker */
 	
-	var title = "Bouncy Ball (2016)";
-	var shortdesc = "An Android game developed by myself.";
-	var longdesc = "<p>The goal was to make a free game where the user can play endlessly but once the user dies in the game, it resets back to the start. " +
-					"This encourage the users to keep coming back to the game and try to beat their own previous highscores. " +
-					"Facebook API was also integrated in the App so the users were able to share their highscores on Social Media to compete with each other. </p>" +
-					"<p>I was responsible for the whole development of the game, including " +
-					"planning the game's framework, developing the App, managing the Developer Account and marketing the game.</p>" +
-					"<p>The game reached about 1000 downloads.</p>" +
+	var title = "Workout Tracker";
+	var shortdesc = "A simple Workout Tracker made with Spring Boot.";
+	var longdesc = "<p></p>" +
 					"<p><label><b>Tech Summary:</b></label>" +
 					"<ul>" +
-					"<li>Language: C# and Java (Android)</li>" +
-					"<li>Game engine: Unity game engine</li>" +
-					"<li>Version control: Git</li></ul></p>";
-	var directory ="projects/bouncyball";
-	var photosName = [ "photo_1.png", "photo_2.png", "photo_3.png", "photo_4.png", "photo_5.png", "photo_6.png" ];
+					"<li>Language: Java</li>" +
+					"<li>Framework: Spring Boot</li>" +
+					"<li>Version control: Git</li></ul></p>";	
+	var directory = "projects/workouttracker";
+	var photosName = [ "screenshot1.jpg", "screenshot2.jpg" ];
 	
 	addProject(0, title, shortdesc, longdesc, directory, photosName);
 	
@@ -63,41 +53,51 @@ function onStart() {
 	var directory ="projects/gameofmoves";
 	var photosName = [ "photo_1.png", "photo_2.png", "photo_3.png", "photo_4.png", "photo_5.png", "photo_6.png", "photo_7.png" ];
 	
-	addProject(0, title, shortdesc, longdesc, directory, photosName);
+	addProject(1, title, shortdesc, longdesc, directory, photosName);
 	
-	/* URL Shortener */
+	/* Bouncy Ball */
 	
-	var title = "URL Shortener";
-	var shortdesc = "A simple URL Shortener made with Spring Boot.";
-	var longdesc = "<p></p>" +
+	var title = "Bouncy Ball (2016)";
+	var shortdesc = "An Android game developed by myself.";
+	var longdesc = "<p>The goal was to make a free game where the user can play endlessly but once the user dies in the game, it resets back to the start. " +
+					"This encourage the users to keep coming back to the game and try to beat their own previous highscores. " +
+					"Facebook API was also integrated in the App so the users were able to share their highscores on Social Media to compete with each other. </p>" +
+					"<p>I was responsible for the whole development of the game, including " +
+					"planning the game's framework, developing the App, managing the Developer Account and marketing the game.</p>" +
+					"<p>The game reached about 1000 downloads.</p>" +
 					"<p><label><b>Tech Summary:</b></label>" +
 					"<ul>" +
-					"<li>Language: Java</li>" +
-					"<li>Framework: Spring Boot</li>" +
+					"<li>Language: C# and Java (Android)</li>" +
+					"<li>Game engine: Unity game engine</li>" +
 					"<li>Version control: Git</li></ul></p>";
-	var directory = "projects/urlshortener";
-	var photosName = [ "screenshot.jpg" ];
+	var directory ="projects/bouncyball";
+	var photosName = [ "photo_1.png", "photo_2.png", "photo_3.png", "photo_4.png", "photo_5.png", "photo_6.png" ];
 	
 	addProject(1, title, shortdesc, longdesc, directory, photosName);
 	
-	/* Workout Tracker */
+	/* Brick Maniac */
 	
-	var title = "Workout Tracker";
-	var shortdesc = "A simple Workout Tracker made with Spring Boot.";
-	var longdesc = "<p></p>" +
+	var title = "Brick Maniac (2014)";
+	var shortdesc = "An Android game developed in a team of two.";
+	var longdesc =  "<p>The goal was to make a classic brick breaker game which is much more polished than the rest of the similar games in the Play Store. " + 
+					"We made a free version of the game which contained banner ads and limited levels. " + 
+					"Also we had a paid (pro) version which was completely ad free and all the levels were available. </p>" +
+					"<p>I was responsible to plan out the game's framework and to develop half of the game's code. " +
+					"Also, as I was in charge to manage our Play Store's Developer Account I had to build a separate package (free and paid) from the game and upload them.</p>" +
+					"<p>We have reached about 500 downloads.</p>" +
 					"<p><label><b>Tech Summary:</b></label>" +
 					"<ul>" +
-					"<li>Language: Java</li>" +
-					"<li>Framework: Spring Boot</li>" +
-					"<li>Version control: Git</li></ul></p>";	
-	var directory = "projects/workouttracker";
-	var photosName = [ "screenshot1.jpg", "screenshot2.jpg" ];
+					"<li>Language: Java (Android)</li>" +
+					"<li>Game engine: Written from scratch</li>" +
+					"<li>Version control: SVN</li></ul></p>";
+	var directory ="projects/brickmaniac";
+	var photosName = [ "photo_1.jpg", "photo_2.jpg", "photo_3.png"];
 	
 	addProject(1, title, shortdesc, longdesc, directory, photosName);
 }
 
 function addTitle(text) {
-	document.getElementsByTagName("body")[0].innerHTML += "<h1>" + text + "</h1>";
+	document.getElementsByTagName("body")[0].innerHTML += "<div class='main-title'><h1>" + text + "</h1></div>";
 }
 
 function generateProjectGroups(titles) {
