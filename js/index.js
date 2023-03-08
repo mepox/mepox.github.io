d36679bb2d64ff0eb46daab206afda06e5a3d9c2
mepox <21198248+mepox@users.noreply.github.com>
2023-03-08 07:42:12 +0000

    Re-write descriptions

--- js/index.js
+++ js/index.js
@@ -15,12 +15,24 @@ function onStart() {
 	addDivider();
 	
 	/* About */
-	var about = "<p>An enthusiastic Junior Software Developer and Indie Game Developer with years of hands-on " +
+	/*var about = "<p>An enthusiastic Junior Software Developer and Indie Game Developer with years of hands-on " +
 				"experience in various (personal and commercial) projects.</p>" +				 
 				"<p>Experience in coding and using different programming languages like Java, C#, " + 
 				"Python (for scripting), JavaScript, HTML and CSS. " + 
 				"I am passionate about developing software and games. " +
-				"I am self-motivated and enjoy learning about new technologies and frameworks.</p>";	
+				"I am self-motivated and enjoy learning about new technologies and frameworks.</p>";*/
+	var about = "<p>As an enthusiastic junior software developer and indie game developer, " +
+				"I bring years of hands-on experience in various personal and commercial projects to every role I undertake.</p>" +
+
+				"<p>With extensive experience coding in multiple programming languages, including Java, C#, Python (for scripting), " +
+				"JavaScript, HTML, and CSS, I am passionate about creating high-quality software and games that deliver an exceptional user experience.</p>" +
+
+				"<p>Self-motivated and always eager to learn about new technologies and frameworks, " +
+				"I thrive on challenges and enjoy working collaboratively with others to develop innovative solutions that meet and exceed expectations.</p>" +
+
+				"<p>Whether working on solo projects or as part of a team, I take pride in my ability to deliver quality results within tight deadlines. " +
+				"With a keen eye for detail and a passion for delivering excellence, I am committed to creating software and games that inspire and delight users.</p>";
+
 	addBoxAndTitle("About", about);	
 	
 	/* Skills */	
@@ -71,7 +83,7 @@ function onStart() {
 	
 	var title = "FitBuddy";
 	var shortdesc = "Workout Tracker app made with Spring Boot.";
-	var longdesc =	"<ul class='longdesc-list'>" +
+	var longdesc =	/*"<ul class='longdesc-list'>" +
 					"<li>Open source project on GitHub</li>" +
 					"<li>Managed and delegated tasks to contributors</li>" +
 					"<li>Implemented REST API design to handle the clients’ (GET, POST, PUT, DELETE) requests</li>" +
@@ -79,7 +91,18 @@ function onStart() {
 					"<li>Integrated Spring Boot Security to manage the users authentication</li>" +
 					"<li>Frontend made with Bootstrap 5 (JavaScript, HTML and CSS)</li>" +
 					"<li>Features: Users are able to register, login, create exercises and log their workouts</li>" +
-					"</ul>" +
+					"</ul>" +*/
+					"<p>This is an open source workout tracker application built with Spring Boot and available on GitHub. " +
+					"The project is managed collaboratively, with tasks delegated to contributors to ensure high-quality code and functionality.</p>" +
+
+					"<p>The application implements a RESTful API design that handles clients' requests (including GET, POST, PUT, and DELETE) " +
+					"for exercises and workout logs. The project also incorporates the DTO (Data Transfer Object) design pattern " +
+					"to improve efficiency and readability.</p>" +
+
+					"<p>For user security, the application integrates Spring Boot Security, providing robust authentication and authorization features.</p>" +
+
+					"<p>The frontend is built with Bootstrap 5 and uses JavaScript, HTML, and CSS to deliver an intuitive and responsive user experience. " +
+					"Users can register, log in, create exercises, and log their workouts with ease, making it simple to track and manage their fitness goals.</p>"
 
 					"<p>" + 
 					"<a href='https://github.com/mepox/fitbuddy' target='_blank'>View Source Code on GitHub</a></p>" +
@@ -99,12 +122,22 @@ function onStart() {
 	
 	var title = "URL Shortener";
 	var shortdesc = "URL Shortener made with Spring Boot.";
-	var longdesc = 	"<ul class='longdesc-list'>" +
+	var longdesc = 	/*"<ul class='longdesc-list'>" +
 					"<li>Shortens the users' URL</li>" +
 					"<li>Generates an unique key for each URL</li>" +
 					"<li>REST API design to handle the clients' (GET, POST, DELETE) requests</li>" +
 					"<li>Frontend uses plain JavaScript, HTML and CSS</li>" +
-					"</ul>" +				
+					"</ul>" +*/
+					"<p>My open-source URL shortener application, built with Spring Boot and available on GitHub, " +
+					"makes it easy for users to shorten their URLs and share them quickly and conveniently.</p>" +
+
+					"<p>With my application, users can simply enter their long URL, and the system generates a unique key " +
+					"for each URL, making it easier to share and access. I have implemented a REST API design to handle the " +
+					"clients' (GET, POST, DELETE) requests, ensuring seamless integration with other systems.</p>" +
+
+					"<p>Frontend uses plain JavaScript, HTML, and CSS, providing a clean, user-friendly interface that's easy " +
+					"to navigate and understand. Whether you're looking to shorten a URL for a social media post or share a link " +
+					"with friends, my URL shortener application provides a fast, reliable, and secure solution that's always at your fingertips.</p>" +
 					
 					"<p><a href='https://github.com/mepox/simple-urlshortener' target='_blank'>" + 
 					"View Source Code on GitHub</a></p>";
@@ -120,7 +153,7 @@ function onStart() {
 	
 	var title = "Game Of Moves";
 	var shortdesc = "Commercial Android game developed in a team of two.";
-	var longdesc = "<p>The goal was to make a game which is challenging but also slow paced which is much more suited " +
+	var longdesc =  /*"<p>The goal was to make a game which is challenging but also slow paced which is much more suited " +
 					"for a mobile game. </p>" +
 					"<p><b>Game Of Moves</b> is a multi-level puzzle game where the players has to move all the boxes to the " + 
 					"destination zones however each boxes has limited moves. </p>" +
@@ -133,7 +166,21 @@ function onStart() {
 					"<li>Managed the Google Play's Developer Console</li>" +
 					"</ul>" +
 					
-					"<p>The game reached about 500 downloads.</p>" +
+					"<p>The game reached about 500 downloads.</p>" +*/
+					"<p>Game of Moves is a challenging but relaxed multi-level puzzle game designed specifically for mobile devices. " +
+					"Players must move all the boxes to their respective destination zones, but each box has a limited number of moves, " +
+					"making the game both thought-provoking and engaging.</p>" +
+
+					"<p>Built using the Unity game engine, Game of Moves features intuitive gameplay and a carefully crafted design that " +
+					"challenges players at every turn. The core gameplay mechanics were designed and programmed by our team, " +
+					"while we also assisted with level design to ensure each level offered a unique and engaging challenge.</p>" +
+
+					"<p>We also integrated the Facebook API, so players can share their progress and achievements with friends and family " +
+					"on social media. </p>" +
+
+					"<p>Game of Moves has been well received, with approximately 500 downloads to date. It's a game that offers both " +
+					"entertainment and intellectual stimulation, making it perfect for anyone looking for a mobile game that's both fun and challenging.</p>" +
+
 					"<p>" + 
 					"<a href='https://play.google.com/store/apps/details?id=com.pixelbits.thegameofmoves' " + 
 					"target='_blank'><img alt='Get it on Google Play' src='logos/googleplay.png' " + 
@@ -151,7 +198,7 @@ function onStart() {
 	
 	var title = "Bouncy Ball";
 	var shortdesc = "Commercial Android game developed by myself.";
-	var longdesc = "<p>The goal was to make an addicting game where the users are keep coming back to try to beat " + 
+	var longdesc =  /*"<p>The goal was to make an addicting game where the users are keep coming back to try to beat " + 
 					"their previous highscores. </p>" +
 					"<p><b>Bouncy Ball</b> is an action game, where the players has to jump higher and higher by " + 
 					"tapping the screen and avoid the dangerous spikes. </p>" +
@@ -166,7 +213,20 @@ function onStart() {
 					"<li>Managed the Google Play's Developer Console</li>" +
 					"</ul>" +										
 					
-					"<p>The game reached about 1000 downloads.</p>" +
+					"<p>The game reached about 1000 downloads.</p>" +*/
+					"<p>Bouncy Ball is an addictive action game designed to keep players coming back for more, as they " +
+					"strive to beat their previous high scores. The game challenges players to jump higher and higher by " +
+					"tapping the screen, while avoiding dangerous spikes that threaten to bring their journey to an abrupt end.</p>" +
+
+					"<p>Built using the Unity game engine, Bouncy Ball is a casual yet engaging action game. I planned and developed " +
+					"the core gameplay, designed the game mechanics, and created the game's assets to ensure a fun and satisfying experience for players.</p>" +
+
+					"<p>We also integrated the Facebook API, so players can share their progress and achievements on social media, " +
+					"making it easy to show off their high scores and challenge friends to beat them. </p>" +
+
+					"<p>Bouncy Ball has been a hit with players, with approximately 1000 downloads to date. It's a game that combines action and strategy, " +
+					"making it perfect for anyone looking for a fun and engaging mobile gaming experience.</p>" +
+
 					"<p><a href='https://play.google.com/store/apps/details?id=com.laszlojanku.bouncyball' " + 
 					"target='_blank'><img alt='Get it on Google Play' src='logos/googleplay.png' " + 
 					"class='googleplaylogo'/></a>" +
@@ -183,8 +243,8 @@ function onStart() {
 	
 	var title = "Brick Maniac";
 	var shortdesc = "Commercial Android game developed in a team of two.";
-	var longdesc =  "<p>The goal was to make a classic brick breaker game which is much more polished than the rest " + 
-	"of the similar games in the Play Store. </p>" +				 
+	var longdesc =  /*"<p>The goal was to make a classic brick breaker game which is much more polished than the rest " + 
+					"of the similar games in the Play Store. </p>" +				 
 					"<p><b>Brick Maniac</b> is a classic brick breaker game, where the players has to destroy all " + 
 					"the bricks with the ball to complete the levels. </p>" +
 					
@@ -195,7 +255,19 @@ function onStart() {
 					"<li>Managed the Google Play's Developer Console</li>" +
 					"</ul>" +
 					
-					"<p>The game reached about 500 downloads.</p>" +
+					"<p>The game reached about 500 downloads.</p>" +*/
+					"<p>With a goal of creating a classic brick breaker game that surpassed the quality of similar titles in the " +
+					"Play Store, we developed Brick Maniac – a thrilling game that challenges players to destroy all the bricks " +
+					"with a ball to complete each level.</p>" +
+
+					"<p>We designed the core gameplay and programmed the main game logic, engineering a custom game engine to ensure "+
+					"a smooth and engaging experience for players. We also integrated the Facebook API, enabling users to share " +
+					"their progress and achievements on social media and inviting friends to join in the fun.</p>" +
+
+					"<p>With approximately 500 downloads to date, Brick Maniac has been a hit with players who enjoy the challenge " +
+					"of classic arcade-style games. Whether looking to kill time or hone their skills, Brick Maniac offers " +
+					"endless hours of entertainment and fun.<p>" +
+
 					"<p><a href='https://play.google.com/store/apps/details?id=com.tupix.brickmaniacfree' " + 
 					"target='_blank'><img alt='Get it on Google Play' src='logos/googleplay.png' " +
 					"class='googleplaylogo'/></a>" +
